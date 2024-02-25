b1c4471b00c922980b2258009bc3dfbaa616fdfe
Ömer Faruk Ekim <o.farukekimofficial@gmail.com>
2024-02-25 16:30:41 +0300

    Register ve Login ekranları kullanılabilir hale getirildi

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,40 +1,113 @@
+import 'package:bee_store/pages/register_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  var _yukleniyor = false;
+  var _loading = false;
+  var _errorMessage = "";
+  var _email = "";
+  var _password = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment
-            .center, // bu zaten column da varsayılan oalrak böyle ama bu yatayda düşünüyor onu
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-              child: Text(
-                  "Login Page")), // burdaki centerda gerekli sayfanın tam en ortasına gelmesi için
-          if (_yukleniyor)
-            CircularProgressIndicator()
-          else
+      appBar: AppBar(title: Center(child: Text("LOGIN PAGE"))),
+      body: Padding(
+        padding: const EdgeInsets.all(48.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_errorMessage.isNotEmpty)
+              Text(
+                "ERROR: $_errorMessage ",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            TextField(
+              decoration: InputDecoration(hintText: "Enter your email"),
+              keyboardType: TextInputType.emailAddress,
+              onChanged: (value) {
+                _email = value;
+
+                if (_errorMessage.isNotEmpty) {
+                  _errorMessage = "";
+                  setState(() {});
+                }
+              },
+            ),
+            SizedBox(height: 4),
+            TextField(
+              decoration: InputDecoration(hintText: "Enter your password"),
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              onChanged: (value) {
+                _password = value;
+
+                if (_errorMessage.isNotEmpty) {
+                  _errorMessage = "";
+                  setState(() {});
+                }
+              },
+            ),
+            if (_loading)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(),
+              )
+            else
+              TextButton(
+                onPressed: () {
+                  final regexExp = RegExp(
+                      r"^[A-Za-z0-9._+\-\']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$");
+                  final isValid =
+                      regexExp.hasMatch(_email) && _password.length > 5;
+                  if (isValid) {
+                    _loading = true;
+                    setState(() {});
+                    FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
+                            email: _email, password: _password)
+                        .catchError((errorMessage) {
+                      _errorMessage = errorMessage.toString();
+                      _loading = false;
+                      setState(() {});
+                    });
+                  } else {
+                    _errorMessage = "Invalid email or password!";
+                    setState(() {});
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 6.0),
+                  child: Text("Login"),
+                ),
+              ),
+            Divider(),
             TextButton(
               onPressed: () {
-                _yukleniyor =
-                    true; // bunları yapma sebebimiz çoklu tıklamanın önüne geçmek
-                setState(() {}); // tam olarak bu ne bak
-                FirebaseAuth.instance.signInAnonymously();
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) {
+                      return RegisterPage();
+                    },
+                  ),
+                );
               },
-              child: Text("LOGİN"),
-            ) // buttonların şekillerni öğren
-        ],
+              child: Text("Register"),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,142 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  var _loading = false;
+  var _errorMessage = "";
+  var _nameSurname = "";
+  var _email = "";
+  var _password = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("REGİSTER PAGE"))),
+      body: Padding(
+        padding: const EdgeInsets.all(48.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment
+              .center, // bu zaten column da varsayılan oalrak böyle ama bu yatayda düşünüyor onu
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_errorMessage.isNotEmpty)
+              Text(
+                "ERROR: $_errorMessage ",
+                style: TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 22),
+              ),
+            TextField(
+              decoration:
+                  InputDecoration(hintText: "Enter your name and surname"),
+              keyboardType: TextInputType
+                  .name, // bu sayede uygulamaya girerken defult kayboard açılmıyorda @ olan açılıyor güzelmiş cidden
+              onChanged: (value) {
+                _nameSurname = value;
+                setState(() {});
+                if (_errorMessage.isNotEmpty) {
+                  _errorMessage = "";
+                }
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "E-mail adresini gir"),
+              keyboardType: TextInputType
+                  .emailAddress, // bu sayede uygulamaya girerken defult kayboard açılmıyorda @ olan açılıyor güzelmiş cidden
+              onChanged: (value) {
+                _email = value;
+                setState(() {});
+                if (_errorMessage.isNotEmpty) {
+                  _errorMessage = "";
+                }
+              },
+            ),
+            SizedBox(
+              height: 4,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Şifreni gir"),
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+              onChanged: (value) {
+                _password = value;
+                setState(() {});
+                if (_errorMessage.isNotEmpty) {
+                  _errorMessage = "";
+                }
+              },
+            ),
+            if (_loading)
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: CircularProgressIndicator(),
+              )
+            else
+              TextButton(
+                onPressed: () {
+                  final regexExp = RegExp(
+                      r"^[A-Za-z0-9._+\-\']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$");
+                  final isValid =
+                      regexExp.hasMatch(_email) && _password.length > 5;
+                  if (isValid && _nameSurname.isNotEmpty) {
+                    _loading = true;
+                    // bunları yapma sebebimiz çoklu tıklamanın önüne geçmek
+                    setState(() {}); // tam olarak bu ne bak
+                    // email validation kodları var doğruluğu kontrol etemk için bak istersen
+                    FirebaseAuth.instance
+                        .createUserWithEmailAndPassword(
+                            email: _email, password: _password)
+                        .catchError(
+                      (errorMessage) {
+                        _errorMessage = errorMessage.toString();
+                        _loading = false;
+                        setState(() {});
+                      },
+                    ).then((value) async {
+                      final uid = value.user?.uid;
+                      final userData = {
+                        "name": _nameSurname,
+                        "email": _email,
+                        // "password": _password // tehlikeli dendi
+                        "registerDate": FieldValue.serverTimestamp(),
+                      };
+                      if (uid == null) {
+                        await FirebaseFirestore.instance
+                            .collection("users")
+                            .add(userData);
+                      } else {
+                        await FirebaseFirestore.instance
+                            .collection("users")
+                            .doc(uid)
+                            .set(
+                                userData); // bir dosyayı değiştirmek için iki tane deerimiz var bi update bide set methodlarımız update sadece ilgili bölümü değiştirir ve önceden var olması gerekir set de ise yoksa kendisi oluşturur
+                      }
+                      if (mounted)
+                        Navigator.of(context)
+                            .pop(); // önceki sayfaya gideceğiz // monted var mı yok mu onun için var
+                    });
+                  } else {
+                    _errorMessage =
+                        "All e-mail address, password, name and surname information is required";
+                    setState(() {});
+                  }
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 6.0),
+                  child: Text("Register"),
+                ),
+              ), // buttonların şekillerni öğren
+          ],
+        ),
+      ),
+    );
+  }
+}
